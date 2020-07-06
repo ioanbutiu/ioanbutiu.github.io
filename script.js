83588f8399b8bc67a23de3f59d01dd24d434fba1
Ioan Butiu <ioanbutiu@Ioans-MacBook-Pro.local>
2020-07-05 23:46:06 -0700

    0.005

--- script.js
+++ script.js
@@ -1,154 +1,88 @@
-
-setInterval(makeRainbowBackground,3000);
-
-function makeRainbowBackground(){
-
-var r = getRandomInt(200, 255);
-var g = getRandomInt(200, 255);
-var b = getRandomInt(200, 255);
-
-var r2 = getRandomInt(200, 255);
-var g2 = getRandomInt(200, 255);
-var b2 = getRandomInt(200, 255);
-
-document.body.style.backgroundImage = "linear-gradient(rgb("+r+","+g+","+b+"), rgb("+r2+","+g2+","+b2+")";
-
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-
-
-    /*$("body").children().each( function (i) {
-
-      if ($(this).prop("id") === "light-switch") {
-
-      } else {
-        console.log( $(this).prop("tagName"));
-        $(this).hide();
-      }
-    });*/
-    /*for (i = 0; i <= $("*").length; i++ ) {
-      if ($("*")[i] === $("#light-switch")) {
-      } else {
-        $("this").hide();
-      }
-    }
-  }*/
-
-  var hidden = false;
+var dark = false;
+var viewHidden = true;
+var projectsHidden = true;
 
 $(document).ready(function(){
 
-  $("#light-switch").html("<img src='images/zap-off.svg'>");
+  $("#filter-links-view").css("display", "none");
+  $("#filter-links-projects").css("display", "none");
+
+
+  $("#view-button").on("click", function () {
+    if (viewHidden) {
+      $("#filter-links-view").css("display", "inline-block");
+      $("#view-button").css("margin-left", "2rem");
+      viewHidden = false;
+    } else {
+      $("#filter-links-view").css("display", "none");
+      $("#view-button").css("margin-left", "0.5rem");
+      viewHidden = true;
+    }
+  });
+
+  $("#projects-button").on("click", function () {
+    if (projectsHidden) {
+      $("#filter-links-projects").css("display", "inline-block");
+      $("#projects-button").css("margin-left", "2rem");
+      projectsHidden = false;
+    } else {
+      $("#filter-links-projects").css("display", "none");
+      $("#projects-button").css("margin-left", "0.5rem");
+      projectsHidden = true;
+    }
+  });
+
+  $("#project-link-preview-team").hover(function() {
+    $("#project-hover-image-team").css("display", "block");
+  }, function() {
+    $("#project-hover-image-team").css("display", "none");
+  });
 
   $("#light-switch").on("click", function () {
-    if (hidden == false) {
-      hideSite();
+    if (dark == false) {
+      darkMode();
+      dark = true;
     } else {
-      showSite ();
+      lightMode ();
+      dark = false;
     }
   });
 });
 
-function hideSite () {
-    console.log( "the page is hidden" );
-    hidden = true;
+function darkMode () {
+  $("body").css("background-color", "black");
+  $("#nav-bar").css("background-color", "black");
+  $("#project-list-heading-container").css("background-color", "black");
+  $("*").css("color", "white");
+  $("*").css("border-color", "white");
+  $(".nav-links").css("background-color", "black")
 
-    var elements = $("body").children();
+  $(".nav-links").hover(function(){
+    $(this).css({"background-color":"white", "color":"black"});
+  }, function(){
+    $(this).css({"background-color":"black", "color":"white"});
+  });
+  $("#light-switch").css("background-color", "white");
+  $("#light-switch").css("color", "black");
 
-    elements.each( function (i) {
-
-      if ($(this).prop("id") === "light-switch") {
-
-      } else {
-        $(this).hide();
-      }
-
-    });
-
-    $("#light-switch").html("<img src='images/zap.svg'>");
-
-  }
-
-
-function showSite () {
-    console.log( "the page is shown" );
-    hidden = false;
-
-    var elements = $("body").children();
-
-    elements.each( function (i) {
-
-      if ($(this).prop("id") === "light-switch") {
-
-      } else {
-        $(this).show();
-      }
-
-    });
-
-    $("#light-switch").html("<img src='images/zap-off.svg'>");
-
-  }
-
-/*$(document).ready(function(){
-  console.log($("title-container"));
-
-  console.log($("li"));
-  console.log("There are " + $("li").length + " li tags");
-});*/
-
-
-
-/*$(document).ready(function(){
-  console.log($("title-container"));
-
-  console.log($("li"));
-  console.log("There are " + $("li").length + " li tags");
-});*/
-
-
-
-/*var elem = document.getElementsByTagName('div');
-console.log(elem);
-
-window.onload = function(){
-
-  for(i = 0; i < elem.length; i++) {
-    elem[i].classList.add('hidden-div')
-    console.log(i + ' div hidden');
-  }
-
-  for(let i = 0; i < elem.length; i++) {
-    setTimeout(function() {elem[i].classList.remove('hidden-div')}, i * 500);
-    console.log(i + ' div revealed');
-  }
-
-};
-
-
-function revealDiv(i) {
-  elem[i].classList.remove('hidden-div')
-}*/
-
-/*setInterval(makeRainbowBackground,2000);
-
-
-function makeRainbowBackground(){
-var r = getRandomInt(200, 255);
-var g = getRandomInt(200, 255);
-var b = getRandomInt(200, 255);
-
-document.body.style.backgroundColor = "rgb("+r+","+g+","+b+")";
-
+  $("#light-switch").html("LIGHT");
 }
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}*/
+
+function lightMode () {
+  $("body").css("background-color", "white");
+  $("#nav-bar").css("background-color", "white");
+  $("#project-list-heading-container").css("background-color", "white");
+  $("*").css("color", "black");
+  $("*").css("border-color", "black");
+  $(".nav-links").css("background-color", "white")
+
+  $(".nav-links").hover(function(){
+    $(this).css({"background-color":"black", "color":"white"});
+  }, function(){
+    $(this).css({"background-color":"white", "color":"black"});
+  });
+  $("#light-switch").css("background-color", "black");
+  $("#light-switch").css("color", "white");
+
+  $("#light-switch").html("DARK");
+}
